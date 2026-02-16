99ffa3990ade98bf9ad6dab20a0916d55b06b8c1
VishnuVikash-droid <vishnuvikashak9@gmail.com>
2026-02-16 18:13:12 +0530

    File Added

--- tests/Day2.spec.js
+++ tests/Day2.spec.js
@@ -9,16 +9,16 @@
 // });
 
 
-// import {test , expect} from '@playwright/test'
+import {test , expect} from '@playwright/test'
 
-// test('Login Check' , async ({page}) =>{
-//     await page.goto('https://www.instagram.com/accounts/login/')
-//     await page.locator('//input[@autocomplete="username webauthn"]').fill("Vishnu_appiyan")
-//     await page.locator('//input[@type="password"]').fill('Vishnu@123')
-//     await page.locator('//span[text()="Log in"]').click()
-//     await page.waitForTimeout(3000)
+test('Login Check' , async ({page}) =>{
+    await page.goto('https://www.instagram.com/accounts/login/')
+    await page.locator('//input[@autocomplete="username webauthn"]').fill("Vishnu_appiyan")
+    await page.locator('//input[@type="password"]').fill('Vishnu@123')
+    await page.locator('//span[text()="Log in"]').click()
+    await page.waitForTimeout(3000)
     
-// })
+})
 
 
 
